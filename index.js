cd03335efe573ad56475f0bd558996a3ddd7cf81
chigbuce <42652528+chigbuce@users.noreply.github.com>
2020-03-26 18:23:06 +0100

    Added courses

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let name = "Christian";
+let name = "Christian Chigbu";
+let Courses = "HTML, CSS, Javascript";
 console.log("My name is: " + name);
+console.log("My courses are:" + courses);
 const data = [];
-for (let i=0; i<=200; i++) {
+for (let i=1; i<=200; i++) {
   if (i%2===0) {
     data.push(i);
   }
